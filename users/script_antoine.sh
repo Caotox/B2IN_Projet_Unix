4400878d7af0f683981db532306a9619d8e5ca25
Antoine <arocq92@gmail.com>
2024-11-30 18:04:18 +0100

    exo1

--- users/script_antoine.sh
+++ users/script_antoine.sh
@@ -1,39 +1,45 @@
 #!/bin/bash
 # exo 1
+#!/bin/bash
+
 fichier="Names.txt"
-var_indice=0
 declare -a tableau_indi
+var_indice=0
+
 while read line; do
-    var_ligne=$line
     tableau_indi[$var_indice]="$line"
-    var_indice=$(($var_indice + 1))
+    var_indice=$((var_indice + 1))
 done < "$fichier"
-echo "${tableau_indi[@]}"
-ecart_tabl=0
+
+echo "Liste des utilisateurs : ${tableau_indi[@]}"
+
 longueur_tab=${#tableau_indi[@]}
-longueur_tab=$(($longueur_tab/4))
-for i in {0..$longueur_tab}; do  
+longueur_tab=$((longueur_tab / 4))
+ecart_tabl=0
+
+for ((i=0; i<longueur_tab; i++)); do
     PASSWORD=$(openssl rand -base64 12)
     echo "Votre mot de passe est : $PASSWORD"
-    user=${tableau_indi[0+$ecart_tabl]}
-    group=${tableau_indi[1+$ecart_tabl]}
-    shell=${tableau_indi[2+$ecart_tabl]}
-    rep=${tableau_indi[3+$ecart_tabl]}
-    if [ "$(getent passwd $user)" ]; then  
-        if [ "$(getent group $group)" ]; then  
-            groupadd $group
-            useradd "$user" --groups "$group" --shell "$shell" --home "$rep" 
-        else
-            useradd "$user" --groups "$group" --shell "$shell" --home "$rep" 
-        fi
-    else
-        if [ "$(getent group $group)" ]; then  
-            groupadd $group
-            usermod "$user" --groups "$group" --shell "$shell" --home "$rep" 
-        else
-            usermod "$user" --groups "$group" --shell "$shell" --home "$rep" 
-        fi
+
+    user=${tableau_indi[0 + ecart_tabl]}
+    group=${tableau_indi[1 + ecart_tabl]}
+    shell=${tableau_indi[2 + ecart_tabl]}
+    rep=${tableau_indi[3 + ecart_tabl]}
+
+    if ! getent group "$group" >/dev/null; then
+        groupadd "$group"
     fi
-    chage -M 30 $user
-    ecart_tabl=$(($ecart_tabl + 4)) 
+
+    if getent passwd "$user" >/dev/null; then
+        usermod -g "$group" -s "$shell" -d "$rep" "$user"
+    else
+        useradd -m -g "$group" -s "$shell" -d "$rep" "$user"
+    fi
+
+    # Définir le mot de passe et l'expiration
+    echo "$user:$PASSWORD" | chpasswd
+    chage -M 30 "$user"
+
+    ecart_tabl=$((ecart_tabl + 4))
 done
+

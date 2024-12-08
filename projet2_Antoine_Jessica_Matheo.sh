3880729c16188a9f323019fcea25aff825dcf2bf
Antoine ROCQ <arocq92@gmail.com>
2024-12-08 19:29:05 +0100

    add partie matheo

--- projet2_Antoine_Jessica_Matheo.sh
+++ projet2_Antoine_Jessica_Matheo.sh
@@ -112,6 +112,39 @@ Veuillez vous connecter pour éviter que votre compte ne soit verrouillé ou sup
     esac
 done
 
+groupadd CEO
+groupmod -c "CEO" CEO
+groupadd Marketing
+groupmod -c "Team responsible for all marketing activities" Marketing
+groupadd ProductOwner
+groupmod -c "Team responsible for managing devs" ProductOwner
+groupadd Devs
+groupmod -c "Team responsible for making products" Devs
+groupadd RH
+groupmod -c "Team responsible for hire new employee" RH
+
+usermod -a -G CEO antoine
+usermod -a -G Devs matheo
+usermod -a -G ProductOwner jessica
+
+# Si un groupe est modifie / un utilisateur est retiré, supprime les groupes vides
+
+
+# Récupère tous les groupes
+for group in $(getent group | cut -d: -f1); do # 'cut -d: -f1' ==> nom du groupe
+  # Si le groupe est vide, alors...
+  if [[ $(getent group "$group" | cut -d: -f4) == "" ]]; then # 'cut -d: -f4' ==> utilisateurs du groupe
+    # Supprime le groupe
+    groupdel "$group"
+    echo "Group '$group' is empty and has been deleted!"
+  else
+    # Group is not empty, skip it
+    echo "Group '$group' is not empty, skipping..."
+  fi
+done
+
+echo "Group cleanup complete."
+
 
 repertoire_rh="/../../rh_directory" #faire des chemins pour les répertoires RH et Direction
 repertoire_direction="/../t../direction_directory"

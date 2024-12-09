7313ba82e18422d46de2242eca4ff0c07b56f3e7
Math_13 <MathVov.91@outlook.fr>
2024-12-09 19:11:34 +0100

    new question function

--- manage_unix.sh
+++ manage_unix.sh
@@ -3,6 +3,15 @@
 # Ce script initialise tous les autres scripts : création des utilisateurs, création des groupes
 
 source src/better_reading.sh # intègre le script 'better_reading.sh' (= récupère toutes ses fonctions et variables)
+create_question() {
+    read -p "$1" response
+    if [[ "$response" == "o" ]]; then
+        return 0  # true
+    else
+        return 1  # False
+    fi
+}
+
 
 utilisateur=$(whoami)
 log_info "Bienvenue administrateur ${BLUE}$utilisateur${RESET} sur le gestionnaire automatisé des utilisateurs et des groupes de l'entreprise '<undefined>'"
@@ -17,10 +26,16 @@ fi
 # Init
 log_info "Initialisation..."
 # Some things to load...
-sudo bash src/init.sh
+# sudo bash src/init.sh
 log_success "Initialisation terminée !"
 echo ""
 echo ""
+
+create_question "Continuer ? (o/n)"
+if [[ $? -eq 1 ]]; then
+    exit 1
+fi
+
 
 # Etape 1
 log_info "Etape 1: Ajouter / modifier des utilisateurs"
@@ -29,12 +44,24 @@ log_success "Processus terminé avec succès."
 echo ""
 echo ""
 
+create_question "Continuer ? (o/n)"
+if [[ $? -eq 1 ]]; then
+    exit 1
+fi
+
+
 # Etape 2
 log_info "Etape 2: Gestion des utilisateurs inactifs"
 sudo bash src/afk_user.sh 90 || { log_error "Erreur inattendu lors de la gestion des utilisateurs."; exit 1; }
 log_success "Processus terminé avec succès."
 echo ""
 echo ""
+
+create_question "Continuer ? (o/n)"
+if [[ $? -eq 1 ]]; then
+    exit 1
+fi
+
 
 # Etape 3
 log_info "Etape 3: Gestion des groupes"
@@ -43,6 +70,12 @@ log_success "Processus terminé avec succès."
 echo ""
 echo ""
 
+create_question "Continuer ? (o/n)"
+if [[ $? -eq 1 ]]; then
+    exit 1
+fi
+
+
 # Etape 4
 log_info "Etape 4: Gestion des ACL (Access Control List)"
 sudo bash src/manage_acl.sh || { log_error "Erreur inattendu lors de la gestion des ACL."; exit 1; }
@@ -50,10 +83,16 @@ log_success "Processus terminé avec succès."
 echo ""
 echo ""
 
+create_question "Continuer ? (o/n)"
+if [[ $? -eq 1 ]]; then
+    exit 1
+fi
+
+
 # Etape 5 (BONUS)
 log_info "Etape 5: Rapport"
-read -p "Voulez-vous générer un rapport ? (o/n)" response
-if [[ "$response" == "o" ]]; then
+create_question "Voulez-vous générer un rapport ? (o/n)"
+if [[ $? -eq 1 ]]; then
     sudo bash src/logs.sh || { log_error "Erreur inattendu lors de la création du rapport."; exit 1; }
     log_info "Rapport rédigé : ${YELLOW}'./logs.log'${RESET}"
 else

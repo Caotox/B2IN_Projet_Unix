8fa08394ef8816bfa8519c806081ccd71e48e046
Math_13 <MathVov.91@outlook.fr>
2024-12-09 18:57:42 +0100

    group creation

--- src/init.sh
+++ src/init.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
+
+create_group() {
+    group_name=$1
+    description=$2
+
+    # Vérifier si le groupe existe
+    if getent group "$group_name" > /dev/null 2>&1; then
+        echo "Le groupe '$group_name' existe déjà."
+        # Modifier la description si nécessaire
+        sudo groupmod -c "$description" "$group_name"
+        echo "Description du groupe '$group_name' modifiée."
+    else
+        # Créer le groupe si il n'existe pas
+        echo "Création du groupe '$group_name'..."
+        sudo groupadd "$group_name"
+        sudo groupmod -c "$description" "$group_name"
+        echo "Le groupe '$group_name' a été créé et sa description ajoutée."
+    fi
+}
+
 # Création des groupes
-groupadd CEO
-groupmod -c "CEO" CEO
-groupadd Marketing
-groupmod -c "Team responsible for all marketing activities" Marketing
-groupadd ProductOwner
-groupmod -c "Team responsible for managing devs" ProductOwner
-groupadd Devs
-groupmod -c "Team responsible for making products" Devs
-groupadd RH
-groupmod -c "Team responsible for hire new employee" RH
+create_group_if_not_exists "CEO" "CEO"
+create_group_if_not_exists "Marketing" "Team responsible for all marketing activities"
+create_group_if_not_exists "ProductOwner" "Team responsible for managing devs"
+create_group_if_not_exists "Devs" "Team responsible for making products"
+create_group_if_not_exists "RH" "Team responsible for hiring new employees"
 
 
 # installation des packages utiles

b3acdf731c1851c6351ffe6a4a6e816c4ddb611d
Math_13 <MathVov.91@outlook.fr>
2024-12-10 04:55:10 +0100

    rapport (bonus)

--- manage_unix.sh
+++ manage_unix.sh
@@ -89,11 +89,11 @@ log_info "Etape 5: Rapport"
 create_question "Voulez-vous générer un rapport ? (o/n): "
 if [[ $? -eq 0 ]]; then
 
-    echo -e "Ecrivez un nom de chemin valide ( ${YELLOW}rapport.log${RESET} par défaut )"
+    echo -e "Ecrivez un nom de chemin valide ( ${YELLOW}rapport.txt${RESET} par défaut )"
     read -p "> chemin: " path
     # Si la réponse est vide, définir une valeur par défaut
     if [[ -z "$path" ]]; then # -z vérifie la longueur nulle
-        path="rapport.log"
+        path="rapport.txt"
     fi
 
     sudo bash src/rapport.sh $path || { log_error "Erreur inattendu lors de la création du rapport."; exit 1; }
--- src/rapport.sh
+++ src/rapport.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "$1"
+# Définir les variables
+DATE=$(date '+%Y-%m-%d')
+RAPPORT="$1"
+DUREE_INACTIVITE=30 # Durée en jours pour détecter les utilisateurs inactifs
+
+# Créer un en-tête pour le rapport
+echo "Rapport Système - Généré le $DATE" > $RAPPORT
+echo "-----------------------------------" >> $RAPPORT
+echo "" >> $RAPPORT
